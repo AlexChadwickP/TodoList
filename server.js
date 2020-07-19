eb5572cfe4e89a34b034f85de5fa0d66c924fa9c
Alex Chadwick <alexchadwick@outlook.es>
2020-07-19 10:00:05 +0100

    Added initial application

---
+++ server.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
+let app = express();
+
+const urlencondedParser = bodyParser.urlencoded({ extended: false });
+const PORT = 8000;
+
+const url = "mongodb://localhost:27017";
+
+let MongoClient = require('mongodb').MongoClient(url, { useUnifiedTopology: true });
+
+class Todo {
+    constructor(title, description, resolved) {
+        this.title = title;
+        this.description = description;
+        this.resolved = resolved;
+    }
+
+    getJSON() {
+        return {
+            title: this.title,
+            description: this.description,
+            resolved: this.resolved
+        };
+    }
+}
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+
+    const vars = {
+        title: "Home"
+    };
+    res.render('index');
+});
+
+app.get('/todos', (req, res) => {
+
+    let todos;
+
+    MongoClient.connect((err, db) => {
+        if(err) throw err;
+        let dbo = db.db("TodoList");
+        dbo.collection("Todos").find({}).toArray((err, todoArray) => {
+            if (err) throw err;
+            todos = todoArray;
+
+            const vars = {
+                title: "Todos",
+                todos: todos
+            };
+
+            res.render('todos', vars);
+        });
+    });
+});
+
+app.post('/todos', urlencondedParser, (req, res) => {
+    
+    MongoClient.connect((err, db) => {
+        if (err) throw err;
+        let dbo = db.db("TodoList");
+        let todo = { title: req.body.title, description: req.body.description, resolved: false };
+
+        dbo.collection("Todos").insertOne(todo, (err, res) => {
+            if (err) throw err;
+        });
+    });
+
+    res.redirect('todos');
+
+});
+
+app.get('/delete', (req, res) => {
+    console.log(req.query.id);
+
+    MongoClient.connect((err, db) => {
+        if (err) throw err;
+        let dbo = db.db("TodoList");
+        let query = { "_id": ObjectID(req.query.id) };
+        dbo.collection("Todos").deleteOne(query, (err, obj) => {
+            if (err) throw err;
+            console.log(obj.result.n);
+        });
+    });
+
+    res.redirect('/todos');
+});
+
+app.get('*', (req, res) => {
+    const vars = {
+        title: "404 - Not Found"
+    };
+    res.render('404', vars);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running");
+});
